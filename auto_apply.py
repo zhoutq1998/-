04c2a0d5d2c80e5af38ee35da17038cb00121906
zhoutq1998 <81215134+zhoutq1998@users.noreply.github.com>
2022-02-18 16:19:12 +0800

    Update auto_apply.py

--- auto_apply.py
+++ auto_apply.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from datetime import date, timedelta
+import sys
 
-if __name__ == "__main__":
+def run(id, password, day_num):
     data = {
             'url': 'http://thos.tsinghua.edu.cn',
-            'id': '',
-            'pw': ''
+            'id': id,
+            'pw': password
     }
 
     # ====================初始化浏览器=======================
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.headless = False  # True的话就不会跳出网页
+    chrome_options.headless = True  # True的话就不会跳出网页
     chrome = webdriver.Chrome(options=chrome_options)
     chrome.implicitly_wait(10)  # 学校的网页加载太慢，就等一下
 
@@ -20,8 +21,9 @@ if __name__ == "__main__":
     chrome.find_element(By.ID, 'i_user').send_keys(data['id'])
     chrome.find_element(By.ID, 'i_pass').send_keys(data['pw'])
     chrome.find_element(By.CLASS_NAME, 'btn.btn-lg.btn-primary.btn-block').click()
-    for day in range(8):
+    for day in range(day_num):
         apply_date = str(date.today() + timedelta(days=day))
+        print(apply_date)
         # ====================学生进出校审批======================
         chrome.find_element(By.NAME, '学生进出校审批').click()
         chrome.switch_to.frame('formIframe')
@@ -37,4 +39,14 @@ if __name__ == "__main__":
         chrome.find_element(By.NAME, "first_item_1").click()
     chrome.close()
 
-# 徐总，强
+def main():
+    args = sys.argv[1:]
+    id = args[0]
+    passwd = args[1]
+    day_num = 1
+    if len(args) >= 3:
+        day_num = min(int(args[2]), 8)
+    run(id, passwd, day_num)
+
+if __name__ == "__main__":
+    main()
